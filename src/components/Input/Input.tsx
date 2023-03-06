fe0b486afe5b0156c215b82c2674ad16f181bb17
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-06 15:41:42 +0100

    fix: inputs border

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -20,7 +20,7 @@ export const Input = ({ label, value, name, onChange, id }: InputProps) => {
         {label}
       </label>
       <input
-        className="appearance-none block w-full bg-white text-gray-700 border-2 border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+        className="appearance-none block w-full bg-white text-gray-700 border-2 border-zinc-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-300"
         name={name}
         id={id ? id : generatedId}
         type="text"
--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -25,7 +25,7 @@ export const Textarea = ({
         {label}
       </label>
       <textarea
-        className="appearance-none block w-full h-20 bg-white text-gray-700 border-2 border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-300"
+        className="appearance-none block w-full h-20 bg-white text-gray-700 border-2 border-zinc-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-300"
         name={name}
         id={id ? id : generatedId}
         onChange={onChange}

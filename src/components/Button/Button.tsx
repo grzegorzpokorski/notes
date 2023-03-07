56856030d7cb2fc999d74262db282de68ea7d10e
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-07 02:58:59 +0100

    feat: more interactive styles to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,16 +2,16 @@ import type { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
 const defaultStyles =
-  "inline-flex gap-1.5 justify-center items-center rounded text-white disabled:cursor-not-allowed transition-colors disabled:bg-gray-300";
+  "inline-flex gap-1.5 justify-center items-center rounded text-white disabled:cursor-not-allowed transition-colors disabled:bg-gray-300 disabled:border-t-2 disabled:border-b-2 disabled:border-gray-300 border-b-4 active:border-b-0 active:border-t-4";
 type Colors = "default" | "red" | "green" | "github";
 
 type Type = "button" | "submit";
 
 const colors = {
-  default: twMerge("bg-blue-600 hover:bg-blue-800"),
-  red: twMerge("bg-red-500 hover:bg-red-700"),
-  green: twMerge("bg-green-500 hover:bg-green-700"),
-  github: twMerge("bg-zinc-700 hover:bg-zinc-800"),
+  default: twMerge("bg-blue-600 hover:bg-blue-700 border-blue-900"),
+  red: twMerge("bg-red-500 hover:bg-red-700 border-red-900"),
+  green: twMerge("bg-green-500 hover:bg-green-700 border-green-900"),
+  github: twMerge("bg-zinc-700 hover:bg-zinc-800 border-zinc-900"),
 } as const;
 
 type Sizes = "default" | "large";
